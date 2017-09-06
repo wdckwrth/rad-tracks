9ef5bad00cf5690f29fd994a588ce337abae8c98
wdckwrth <will.duckworth@gmail.com>
2017-09-06 14:09:56 -0400

    Additional configs; modifications to main script.

--- radtracks.sh
+++ radtracks.sh
@@ -8,6 +8,7 @@ TITLE='title'
 YEAR=1900
 TRIM_START=0
 TRIM_END=0
+COVER='folder.jpg'
 
 URLS=()
 FILES=()
@@ -23,32 +24,69 @@ while IFS='=' read name value || [[ -n "$line" ]]; do
     if [ "$name" == "TRACK" ]; then TRACK=$value; fi
     if [ "$name" == "TRIM_START" ]; then TRIM_START=$value; fi
     if [ "$name" == "TRIM_END" ]; then TRIM_END=$value; fi
+    #if [ "$name" == "COVER" ]; then COVER=$value; fi
+
 
     if [ "$name" == "FILE" ]; then
-        URLS+=$value;
+        #echo "url: $value"
+        URLS+=( "$value" );
     fi
 done < "$1"
 
 TEMP3=radtracks$TRACK.mp3
+TEMP_COVER=cover$TRACK.jpg
 
-#download files
+if [ ${#URLS[@]} -eq 0 ]; then
+    echo "No URLs to fetch. Exiting."
+    exit 1
+fi
+
+echo ""
+echo "Working on: $ARTIST :: $TITLE"
+echo ""
+
+# download files
+CURCNT=1
+TTLCNT=${#URLS[@]}
 for u in "${URLS[@]}"
 do
-    wget $u
-    FILES+=$(echo ${u} | cut -d/ -f6)
+    echo "fetching: $u ($CURCNT / $TTLCNT)"
+    wget -nv --show-progress $u
+    file=$(echo ${u} | rev | cut -d/ -f1 | rev)
+    FILES+=( "$file" )
+    let CURCNT++
 done
 
+if [ $COVER != "folder.jpg" ]; then
+    echo "fetching: $COVER to $TEMP_COVER"
+    wget -nv -O $TEMP_COVER $COVER
+    COVER=$TEMP_COVER
+fi
+
 # flac to mp3
+echo ""
+echo "converting flac to mp3 ..."
 ffmpeg \
     -safe 0 \
     -y \
+    -loglevel error \
+    -stats \
     -f concat \
     -i <( for f in "${FILES[@]}"; do echo "file '$(pwd)/$f'"; done ) \
     -codec:a libmp3lame \
     -qscale:a 2 \
     $TEMP3
 
-# trim start and end here ...
+# trim file if indicated ...
+if [ $TRIM_START != "0" ] || [ $TRIM_END != "0" ]; then
+    if [ $TRIM_END == "0" ]; then TRIM_END=$(ffprobe -i $TEMP3 -show_entries format=duration -v quiet -of csv="p=0"); fi
+    echo "trim start: $TRIM_START"
+    echo "trim end  : $TRIM_END"
+    ffmpeg -loglevel error -stats -i $TEMP3 -ss $TRIM_START -to $TRIM_END -c copy radtracks$TRACK-trimmed.mp3
+    rm -fv $TEMP3
+    mv radtracks$TRACK-trimmed.mp3 $TEMP3
+fi
+
 
 # add id3 tags
 eyeD3 \
@@ -60,14 +98,14 @@ eyeD3 \
     -A "$ALBUM" \
     -a "$ARTIST" \
     --to-v2.3 \
-    --to-v1.1 \
     --remove-all \
-    --add-image folder.jpg:FRONT_COVER \
+    --add-image $COVER:FRONT_COVER \
     --rename '$track:num $title' \
     $TEMP3
 
+# clean up, clean up, everybody everywhere
 for f in "${FILES[@]}"
 do
     rm -fv $f
 done
-
+rm -fv $TEMP_COVER
